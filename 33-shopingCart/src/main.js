743f588843201788959dbe05c297e77ebbead74c
Shyam3050 <nimmanashyam3050@gmail.com>
2022-10-15 21:46:05 +0530

    new Feature added

--- 33-shopingCart/src/main.js
+++ 33-shopingCart/src/main.js
@@ -4,11 +4,20 @@ const cartItem = document.getElementById("cartItem");
 let basket = JSON.parse(localStorage.getItem("data")) || [];
 
 // functions start
+function addBtn(element) {
+  const id = element.id.slice(0, -1);
+  const search = basket.find((item) => item.id === id);
+  element.innerHTML = ` <i class="bi bi-dash-lg" onclick= "decrement(${id})"></i>
+  <div class="quantity" id = ${id}>${!search ? 0 : search.item}</div>
+  <i class="bi bi-plus-lg" onclick = "increment(${id})"></i>`;
+  const quantityElement = document.getElementById(id);
+  increment(quantityElement);
+}
+
 const generateShop = () => {
   return (shop.innerHTML = shopItemsData
     .map((item) => {
       const { id, name, price, desc, img } = item;
-      const search = basket.find((item) => item.id === id);
       return `<div class="item" id="product-id-${id}">
     <img src="./${img}"  width = "220"alt="clothing store" />
     <div class="details">
@@ -16,10 +25,8 @@ const generateShop = () => {
       <p>${desc}</p>
       <div class="price_quantity">
         <h2>$ ${price}</h2>
-        <div class="buttons">
-          <i class="bi bi-dash-lg" onclick= "decrement(${id})"></i>
-          <div class="quantity" id = ${id}>${!search ? 0 : search.item}</div>
-          <i class="bi bi-plus-lg" onclick = "increment(${id})"></i>
+        <div class="buttons" id = ${id + "1"}>
+          <p onClick = addBtn(${id + "1"})> Add </p>
         </div>
       </div>
     </div>
@@ -30,6 +37,7 @@ const generateShop = () => {
 
 function increment(id) {
   const selectedItem = id;
+  console.log(selectedItem);
   const search = basket.find((item) => item.id === selectedItem.id);
   if (!search) {
     basket.push({
@@ -44,19 +52,30 @@ function increment(id) {
 }
 function decrement(id) {
   const selectedItem = id;
+  console.log(selectedItem);
   const search = basket.find((item) => item.id === selectedItem.id);
-  if (!search || search.item === 0) return;
-  else {
+  if (!search) return;
+  if (search.item === 1) {
+    selectedItem.parentElement.innerHTML = `
+    <p onClick = addBtn(${selectedItem.id + "1"})> Add </p>
+    `;
+    search.item -= 1;
+    
+  } else {
     search.item -= 1;
   }
   update(selectedItem.id);
-  basket = basket.filter((item)=> item.item !== 0);
+
+  basket = basket.filter((item) => item.item !== 0);
   localStorage.setItem("data", JSON.stringify(basket));
 }
 function update(id) {
   const search = basket.find((item) => item.id === id);
-  document.getElementById(id).innerHTML = search.item;
+  console.log(search.item);
+  // if(search.item)
   calcVal();
+  if(search.item === 0)return;
+  document.getElementById(id).textContent = String(search.item);
 }
 
 function calcVal() {
